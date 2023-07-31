0633a83fbdefbf15ecf5fdc2cbc24217a019bad3
MaicolAntali <maicol.antali.ma@gmail.com>
2023-07-31 14:59:05 +0200

    Fix Javadoc

--- clashJ/src/main/kotlin/com/clashj/model/ClanMemberList.kt
+++ clashJ/src/main/kotlin/com/clashj/model/ClanMemberList.kt
@@ -6,7 +6,7 @@ import com.clashj.model.component.clan.ClanMember
 /**
  * Represents the Clan War Log.
  *
- * @property items List of [ClanWarLogEntry].
+ * @property items List of [ClanMember].
  * @property paging Paging information for pagination. Can be null if there is no pagination.
  */
 data class ClanMemberList(
