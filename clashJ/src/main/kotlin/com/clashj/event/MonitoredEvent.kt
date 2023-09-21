fcde3abdba53e65a0f14fa5543b89f29cce2d7d9
MaicolAntali <maicol.antali.ma@gmail.com>
2023-09-21 13:10:12 +0200

    Changes the `MonitoredEvent` to accept two generic types + fire callbacks
    Adds the internal `executeCallback` to execute a callback.
    Adds the `C` generic type representing the callback type associated with the monitored events.

--- clashJ/src/main/kotlin/com/clashj/event/MonitoredEvent.kt
+++ clashJ/src/main/kotlin/com/clashj/event/MonitoredEvent.kt
@@ -3,23 +3,43 @@ package com.clashj.event
 import com.clashj.model.player.Player
 
 /**
- * A sealed class representing monitored events with generic data type [T].
+ * A sealed class representing monitored events with generic data type [T] and a callback type [C].
  *
- * This sealed class is designed for defining monitored events and their associated change detection logic.
- * It allows the creation of specific event types, each with its own change detection implementation.
+ * This sealed class is designed to define monitored events and their associated change detection logic.
+ * It allows the creation of specific event types, each with its own event firing implementation.
  *
  * @param T The generic type representing the data associated with the monitored events.
+ * @param C The generic type representing the callback type associated with the monitored events.
  */
-sealed class MonitoredEvent<T> {
+sealed class MonitoredEvent<T, C : EventCallback> {
 
     /**
-     * Abstract function to determine whether the monitored event has changed based on cached and current data.
+     * Abstract function to fire the monitored event based on cached and current data.
      *
      * @param cached The cached data of type [T].
      * @param current The current data of type [T].
-     * @return `true` if the event has changed, `false` otherwise.
+     * @param callback The callback of type [C] to be invoked when the event occurs.
      */
-    abstract fun hasChanged(cached: T, current: T): Boolean
+    abstract suspend fun fireCallback(cached: T, current: T, callback: C)
+
+    internal suspend fun <T> executeCallback(cached: T, current: T, callback: C, vararg args: String) {
+        when (callback) {
+            is EventCallback.PlayerCallback.PlayerSimpleCallback -> {
+                if (cached is Player && current is Player) {
+                    callback.callback.invoke(cached, current)
+                }
+            }
+
+            is EventCallback.PlayerCallback.PlayerIterableCallback -> {
+                if (cached is Player && current is Player && args[0].isNotEmpty()) {
+                    callback.callback.invoke(cached, current, args[0])
+                }
+            }
+
+            is EventCallback.ClanCallback -> TODO()
+
+        }
+    }
 
     /**
      * Sealed class representing specific player-related monitored events.
@@ -32,19 +52,21 @@ sealed class MonitoredEvent<T> {
          * This event is fired when there is a change in the number of donations made by a player.
          */
         data object Donations : PlayerEvents() {
-            override fun hasChanged(cached: Player, current: Player): Boolean {
-                return cached.donations < current.donations
+            override suspend fun fireCallback(cached: Player, current: Player, callback: EventCallback.PlayerCallback) {
+                if (cached.donations < current.donations) {
+                    executeCallback(cached, current, callback)
+                }
             }
         }
 
         /**
-         * Represents a player event related to [received donations][Player.donationsReceived].
-         *
-         * This event is fired when there is a change in the number of donations received by a player.
+         * Event fired when a player receives troops
          */
         data object DonationsReceive : PlayerEvents() {
-            override fun hasChanged(cached: Player, current: Player): Boolean {
-                return cached.donationsReceived < current.donationsReceived
+            override suspend fun fireCallback(cached: Player, current: Player, callback: EventCallback.PlayerCallback) {
+                if (cached.donationsReceived < current.donationsReceived) {
+                    executeCallback(cached, current, callback)
+                }
             }
         }
     }
