d678f3609a29cb50fe9a7f4ccbbb7b1d8306f639
MaicolAntali <maicol.antali.ma@gmail.com>
2023-11-13 22:50:34 +0100

    Adds the java `sourceCompatibility` and `targetCompatibility`

--- clashJ/build.gradle.kts
+++ clashJ/build.gradle.kts
@@ -29,10 +29,8 @@ dependencies {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
     withSourcesJar()
 }
 
