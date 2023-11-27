9e6f6fe4ce9143b97bf19560b4272d170ea3223f
MaicolAntali <maicol.antali.ma@gmail.com>
2023-11-27 17:10:34 +0100

    Adds missing kdoc

--- clashJ/src/main/kotlin/io/github/maicolantali/types/api/interfaces/ApiListResponse.kt
+++ clashJ/src/main/kotlin/io/github/maicolantali/types/api/interfaces/ApiListResponse.kt
@@ -3,10 +3,16 @@ package io.github.maicolantali.types.api.interfaces
 import io.github.maicolantali.types.api.model.common.paging.Paging
 
 /**
- * Represents a generic interface for API responses containing a list of items of type [T]
+ * Generic interface for API responses containing a list of items of type [T]
  * along with optional paging information.
  *
+ * This interface is designed to encapsulate the structure of API responses that include
+ * a list of items and information about paging.
+ *
  * @param T The type of items contained in the response list.
+ *
+ * @property items The list of items of type [T] included in the API response.
+ * @property paging Optional paging information. See [Paging].
  */
 interface ApiListResponse<T> {
     val items: List<T>
